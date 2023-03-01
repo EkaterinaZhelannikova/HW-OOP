cbac5efe334d771faef0c46a4c6b41cd7ee1b8c8
Ekaterina Zhelannikova <katya.zhelannikova@mail.ru>
2023-03-01 18:41:06 +0300

    save

---
+++ lesson5/game/StringToDirectionExample.java
@@ -0,0 +1,10 @@
+package lesson5.game;
+
+public class StringToDirectionExample {
+
+    public static void main(String[] args) {
+        Direction direction = Direction.ofString("LEFT");
+        System.out.println(direction);
+    }
+
+}
